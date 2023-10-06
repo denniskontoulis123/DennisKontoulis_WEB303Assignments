416a23309633ad6e35ed665a692e80f492b13390
denniskontoulis123 <w0798883@myscc.ca>
2023-10-06 18:21:27 -0400

    did the ajax one

---
+++ WEB303As03/js/getAJAX.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    getTeamAJAX();
+});
+
+function getTeamAJAX() {
+    $.ajax({
+        url: "team.json",
+        type: "GET",
+        beforeSend: function() {
+            $("#team").html("Loading");
+        },
+        success: function(data) {
+            displayTeamData(data);
+        },
+        error: function(){
+            $("#team").html("Content could not be found.");
+        }
+    });
+}
+
+function DisplayTeamData(data) {
+    $("team").empty(); // dump the stuff like last file
+    $.each(data, function(index, member) { // iterate and show all
+        $("#team").append('<h2>' + member.name + '</h2>');
+        $("#team").append('<h5>' + member.name + '</h5>');
+        $("#team").append('<p>' + member.name + '</p>');
+    });
+}

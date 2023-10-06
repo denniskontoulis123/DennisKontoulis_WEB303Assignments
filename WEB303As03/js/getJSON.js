c1a7780472dca0d3ec92a7be9ca6c46398fdb16b
denniskontoulis123 <w0798883@myscc.ca>
2023-10-06 18:15:17 -0400

    this should be functioning but ill test both at once l8r

---
+++ WEB303As03/js/getJSON.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    getTeamJSON();
+});
+
+function getTeamJSON() {
+    $.getJSON("team.json", function(data) {
+        displayTeamData(data);
+    });
+}
+
+function displayTeamData(data) {
+    $("#team").empty() // dumps the content
+    $.each(data, function(index, member) {
+        $("team").append('<h2>' + member.name +'</h2>');
+        $("team").append('<h5>' + member.position +'</h5>');
+        $("team").append('<p>' + member.bio +'</p>');
+    });
+}

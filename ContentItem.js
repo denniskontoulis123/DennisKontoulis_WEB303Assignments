b3f50b7c728ce3e0c42c895176bec2d3493aca84
denniskontoulis123 <w0798883@myscc.ca>
2023-10-27 16:24:39 -0400

    started my contentitem class and then remembered to have it as its own .js lol

---
+++ ContentItem.js
@@ -0,0 +1,8 @@
+class ContentItem {
+    constructor(id, name, description, categoryGenre) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.categoryGenre = categoryGenre;
+    }
+}

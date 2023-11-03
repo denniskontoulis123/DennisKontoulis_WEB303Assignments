70f90f9acd59ae557d15c2cbb5b90386b8c2c502
denniskontoulis123 <w0798883@myscc.ca>
2023-11-03 11:48:33 -0400

    this should work?? i hope?

--- js/script.js
+++ js/script.js
@@ -2,5 +2,31 @@ $(document).ready(function() {
     // accordion stuff
     $("#accordion h2").click(function () {
         var isPanelOpen = $(this).next().is(":visible");
-    })
+
+        $(this).closest('#accordion').find('.content').slideUp();
+
+        // expand panel if not open
+        if (!isPanelOpen) {
+            $(this).next().slideDown();
+        }
+    });
+
+    // tab functionality
+    $("#tabs ul li a").click(function(e) {
+        e.preventDefault();
+
+        // cuts the class
+        $("#tabs ul li a").removeClass("active");
+
+        // add class
+        $(this).addClass("active");
+
+        // hide tab
+        $(".tab-content").hide();
+
+        // show content
+        $($(this).attr('href')).show();
+    });
+
+    $("#tabs ul li:first-child a").click();
 })
